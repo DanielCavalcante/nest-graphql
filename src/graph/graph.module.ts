d05036ab84c12ff0821a1490b1d38fada2ccc214
Daniel Cavalcante <c0500272@vale.com>
2019-07-24 01:24:17 -0300

    Refactor module

--- src/graph/graph.module.ts
+++ src/graph/graph.module.ts
@@ -1,4 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ProductService } from './services/product.service'
+import { ProductResolver } from './resolvers/product.resolver'
 
 @Module({})
-export class GraphModule {}
+export class GraphModule {
+  providers: [ProductService, ProductResolver]
+}
